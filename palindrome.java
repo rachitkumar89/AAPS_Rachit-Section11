671793c0d7faa152caff9253332f37a534d16fdb
rachitrajput89 <rachitchauhanjsp78@gmail.com>
2025-03-06 12:20:19 +0530

    palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -1,6 +1,20 @@
 import java.util.Scanner;
 
+
 public class palindrome {
+
+    public static boolean twopointer(String s1){
+        int i=0;
+        int l=s1.length()-1;
+        while(i<=l){
+            if(s1.charAt(i)!=s1.charAt(l));
+            
+            i++;
+            l--;
+            return false;
+        }
+        return true;
+    }
 
     public static boolean reverse(String s1){
         String s2=new StringBuilder(s1).reverse().toString();
@@ -10,12 +24,16 @@ public class palindrome {
         return false;
         
     }
-    
+
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the String: ");
         String s1=sc.nextLine();
-        boolean answer=reverse(s1);
-        System.out.println(answer);
+        boolean answer1=reverse(s1);
+        System.out.println(answer1);
+        boolean answer2=reverse(s1);
+        System.out.println(answer2);
+        
+        sc.close();
     }
 }
